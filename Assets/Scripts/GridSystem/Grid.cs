40ff7b40657a33b84d9f4ccfa7d002c32b2756ff
Nic <nicolas@raveyard.de>
2020-01-31 22:28:55 +0100

    added basic grid system

---
+++ Assets/Scripts/GridSystem/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Grid
+{
+    public Dictionary<Vector2Int, Cell> gridDict = new Dictionary<Vector2Int, Cell>();
+    public int gridXSize = 100;
+    public int gridYSize = 100;
+
+
+    public Vector3 GetCellWorldCoords(Vector2Int gridPos)
+    {
+        return new Vector3(gridPos.x + 0.5f, 0, gridPos.y + 0.5f);
+    }
+
+    public Vector2Int GetCellGridCoords(Vector3 worldPos)
+    {
+        return new Vector2Int(Mathf.FloorToInt(worldPos.x), Mathf.FloorToInt(worldPos.z));
+    }
+
+    public void PopulateGrid()
+    {
+        for(int x = 0; x < gridXSize; x++)
+        {
+            for(int y = 0; y < gridYSize; y++)
+            {
+                var vec = new Vector2Int(x, y);
+                gridDict[vec] = new Cell(vec);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridSystem/GridManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new Grid();
+        grid.PopulateGrid();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray, out var hitInfo, LayerMask.GetMask("BackgroundPlane"));
+            //Debug.Log(hitInfo.point);
+            Debug.Log(grid.GetCellGridCoords(hitInfo.point));
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!EditorApplication.isPlaying)
+            return;
+        foreach (var position in grid.gridDict.Keys)
+        {
+            Gizmos.DrawWireCube(grid.GetCellWorldCoords(position), new Vector3(1, 0.1f, 1));
+        }
+    }
+}
